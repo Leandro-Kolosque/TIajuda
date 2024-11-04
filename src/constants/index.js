ecddd02b744e1f0dd5461c3a797b2567f334b506
Leandro-Kolosque <161510235+Leandro-Kolosque@users.noreply.github.com>
2024-11-04 18:26:26 -0300

    Const App + index.js

---
+++ src/constants/index.js
@@ -0,0 +1,28 @@
+export const navigation = [
+    {
+      id: "0",
+      title: "Home",
+      url: "#home",
+    },
+    {
+      id: "1",
+      title: "Sobre",
+      url: "#sobre",
+    },
+    {
+      id: "2",
+      title: "Vagas",
+      url: "#vagas",
+    },
+    {
+      id: "3",
+      title: "Contato",
+      url: "#contato",
+    },
+    {
+      id: "4",
+      title: "Login",
+      url: "#login",
+      onlyMobile: true,
+    },
+  ];
